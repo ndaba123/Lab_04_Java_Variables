cbc81a5a3b0e8881af3182c4638cf6e90de84204
ndaba123 <dabanf@mail.uc.edu>
2025-02-06 00:08:04 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args) {
+        // Integer variables
+        int intOperandA = 5, intOperandB = 13;
+        int intSum, intProduct, intDifference, intQuotient, intModulo;
+
+        // Integer operations
+        intSum = intOperandA + intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+
+        // Display integer results
+        System.out.println("Integer Operations:");
+        System.out.println(intOperandA + " + " + intOperandB + " = " + intSum);
+        System.out.println(intOperandA + " * " + intOperandB + " = " + intProduct);
+        System.out.println(intOperandA + " - " + intOperandB + " = " + intDifference);
+        System.out.println(intOperandA + " / " + intOperandB + " = " + intQuotient);
+        System.out.println(intOperandA + " % " + intOperandB + " = " + intModulo);
+
+        // Double variables
+        double doubleOperandA = 3.5, doubleOperandB = 2.0;
+        double doubleSum, doubleProduct, doubleDifference, doubleQuotient;
+
+        // Double operations
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+
+        // Display double results
+        System.out.println("\nDouble Operations:");
+        System.out.println(doubleOperandA + " + " + doubleOperandB + " = " + doubleSum);
+        System.out.println(doubleOperandA + " * " + doubleOperandB + " = " + doubleProduct);
+        System.out.println(doubleOperandA + " - " + doubleOperandB + " = " + doubleDifference);
+        System.out.println(doubleOperandA + " / " + doubleOperandB + " = " + doubleQuotient);
+
+        // Additional variables
+        double myLunchCost = 12.50;
+        int numOfKids = 2;
+        boolean isRaining = false;
+        double gasPrice = 3.99;
+        int favNumber = 7;
+        double shoeSize = 9.5;
+        int birthMonth = 4;
+        String fullName = "Nattaani Daba";
+
+        // Display additional variables
+        System.out.println("\nAdditional Variables:");
+        System.out.println("Lunch cost: $" + myLunchCost);
+        System.out.println("Number of kids: " + numOfKids);
+        System.out.println("Is it raining? " + isRaining);
+        System.out.println("Gas price: $" + gasPrice);
+        System.out.println("Favorite number: " + favNumber);
+        System.out.println("Shoe size: " + shoeSize);
+        System.out.println("Birth month: " + birthMonth);
+        System.out.println("Full name: " + fullName);
+    }
+}
